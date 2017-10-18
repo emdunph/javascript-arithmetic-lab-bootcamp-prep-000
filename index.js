dba688ff027a916a38183041b979d15875c4afaa
Emilie Dunphy <emilie.dunphy@gmail.com>
2017-10-18 20:58:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 
+var a = 14
+var b = 5
+var n = 11
+
+function add(a, b) {
+  return a += b
+}
+
+function subtract(a, b) {
+  return a -= b
+}
+
+function multiply(a, b) {
+  return a *= b
+}
+
+function divide(a, b) {
+  return a /= b
+}
+
+function inc(n) {
+  n += 5
+}
+
+function dec(n){}
+function makeInt(n){}
+function preserveDecimal(n){}
