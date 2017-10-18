272a4fa9a377b68ccaaf96f88dbc95ea5e9259a3
Emilie Dunphy <emilie.dunphy@gmail.com>
2017-10-18 15:40:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-
-var number = 5
-function add() {
-  number+= 3 // 8
+var number = 10
+ 
+function add5() {
+  number += 5
 }
+ 
+function divideBy3() {
+  number /= 3
+}
+ 
+divideBy3()
+ 
+console.log(number) // 3.333333333335
+ 
+add5()
+ 
+console.log(number) // 8.333333333335
